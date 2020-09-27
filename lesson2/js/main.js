a466a9d894a35cbc36b3a43050fd9722c6707be6
W2TH <esc20003@byui.edu>
2020-09-26 23:28:03 -0500

    updated

---
+++ lesson2/js/main.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let update = document.lastModified;
+
+document.getElementById("CopyRight-year").innerHTML = date.getFullYear();
+document.getElementById("Updated-date").innerHTML = update;
