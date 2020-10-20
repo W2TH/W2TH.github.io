342fb79b92b4f5e90e57e806faa3d5e02da87ab1
W2TH <esc20003@byui.edu>
2020-10-20 15:40:18 -0500

    update

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -4,10 +4,21 @@ const mainnav = document.querySelector(".navigation");
 menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
+/*--------------------------------------------*/
+
 let date = new Date();
 let update = document.lastModified;
 
 document.getElementById("CopyRight-year").innerHTML = date.getFullYear();
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-document.getElementById('Updated-date').innerHTML = new Date().toLocaleDateString('en-gb', options); 
+document.getElementById('Updated-date').innerHTML = new Date().toLocaleDateString('en-gb', options);
+
+/*---------------------------------------------*/
+
+let navigation = document.getElementById('notification');
+let day = date.getDay();
+
+if (day == 5) {
+    navigation.classList.toggle('show');
+}
