d46794af5aa35f0f594a485defc1bf3014575f70
W2TH <esc20003@byui.edu>
2020-12-16 17:52:31 -0500

    update

--- ScootsProject/js/rentals.js
+++ ScootsProject/js/rentals.js
@@ -1,42 +1,10 @@
 fetch("https://w2th.github.io/ScootsProject/data/rentals.json")
     .then((response) => response.json())
     .then((jsObject) => {
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 7; i++) {
             var row = jsObject.data[i];
             var tr = document.createElement("tr");
-            var tr_text = document.getElementById("scooters");
-            for (let word in row) {
-                let td = document.createElement("td");
-                td.textContent = row[word];
-                tr.appendChild(td);
-            }
-            tr_text.appendChild(tr);
-        }
-    });
-
-fetch("https://w2th.github.io/ScootsProject/data/rentals.json")
-    .then((response) => response.json())
-    .then((jsObject) => {
-        for (let i = 3; i < 6; i++) {
-            var row = jsObject.data[i];
-            var tr = document.createElement("tr");
-            var tr_text = document.getElementById("ATV");
-            for (let word in row) {
-                let td = document.createElement("td");
-                td.textContent = row[word];
-                tr.appendChild(td);
-            }
-            tr_text.appendChild(tr);
-        }
-    });
-
-fetch("https://w2th.github.io/ScootsProject/data/rentals.json")
-    .then((response) => response.json())
-    .then((jsObject) => {
-        for (let i = 6; i < 9; i++) {
-            var row = jsObject.data[i];
-            var tr = document.createElement("tr");
-            var tr_text = document.getElementById("jeep");
+            var tr_text = document.getElementById("chart");
             for (let word in row) {
                 let td = document.createElement("td");
                 td.textContent = row[word];
