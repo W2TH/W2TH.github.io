61e5514096768b5b74ef441a171d4f34ff7c2bdf
W2TH <esc20003@byui.edu>
2020-12-16 15:40:00 -0500

    update

--- ScootsProject/js/rentals.js
+++ ScootsProject/js/rentals.js
@@ -13,3 +13,19 @@ fetch("https://w2th.github.io/ScootsProject/data/rentals.json")
             tr_text.appendChild(tr);
         }
     });
+
+fetch("https://w2th.github.io/ScootsProject/data/rentals.json")
+    .then((response) => response.json())
+    .then((jsObject) => {
+        for (let i = 3; i < 6; i++) {
+            var row = jsObject.data[i];
+            var tr = document.createElement("tr");
+            var tr_text = document.getElementById("ATV");
+            for (let word in row) {
+                let td = document.createElement("td");
+                td.textContent = row[word];
+                tr.appendChild(td);
+            }
+            tr_text.appendChild(tr);
+        }
+    });
